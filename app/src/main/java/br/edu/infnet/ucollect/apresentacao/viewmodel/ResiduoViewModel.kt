76ad3571d185df91639197cb6c69082fe300dab9
faspinheiro <faspinheiro@gmail.com>
2019-08-19 01:11:09 -0300

    Residuos colocados

---
+++ app/src/main/java/br/edu/infnet/ucollect/apresentacao/viewmodel/ResiduoViewModel.kt
@@ -0,0 +1,25 @@
+package br.edu.infnet.ucollect.apresentacao.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.edu.infnet.ucollect.dominio.modelos.Residuo
+import br.edu.infnet.ucollect.infraestrutura.repositorios.ResiduoRepository
+
+class ResiduoViewModel: ViewModel(){
+
+    private val _residuoRepository = ResiduoRepository()
+    private var _residuos: MutableLiveData<List<Residuo>>? = null
+
+    fun getResiduos(): LiveData<List<Residuo>>{
+        if(_residuos == null){
+            _residuos = MutableLiveData()
+            val tmpResiduos = _residuoRepository.getResiduos()
+            _residuos!!.postValue(tmpResiduos)
+        }
+
+        return _residuos!!
+    }
+
+
+}
---
+++ app/src/main/java/br/edu/infnet/ucollect/infraestrutura/repositorios/ResiduoRepository.kt
@@ -0,0 +1,17 @@
+package br.edu.infnet.ucollect.infraestrutura.repositorios
+
+import br.edu.infnet.ucollect.R
+import br.edu.infnet.ucollect.dominio.modelos.Residuo
+
+class ResiduoRepository {
+
+    private val residuos = listOf<Residuo>(
+        Residuo(1, "Bateria", "XPTO1", 1, R.drawable.ic_battery_20_black_24dp),
+        Residuo(2, "Computador","XPTO2", 2, R.drawable.ic_computer_black_24dp),
+        Residuo(3, "Teclado","XPTO3", 3, R.drawable.ic_keyboard_black_24dp),
+        Residuo(4, "Mémoria","XPTO4", 4, R.drawable.ic_memory_black_24dp),
+        Residuo(5, "Smartphone","XPTO5", 5, R.drawable.ic_smartphone_black_24dp)
+    )
+
+    fun getResiduos() = residuos
+}
