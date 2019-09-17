2fadd79ae03bbccec121e58ccb8b31be7fec2b47
ucollectapp@gmail.com <eds@2019>
2019-09-16 21:14:53 -0300

    emaillogin update

--- app/src/main/java/br/edu/infnet/ucollect/apresentacao/activities/EmailFormActivity.kt
+++ app/src/main/java/br/edu/infnet/ucollect/apresentacao/activities/EmailFormActivity.kt
@@ -3,16 +3,23 @@ package br.edu.infnet.ucollect.apresentacao.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import br.edu.infnet.ucollect.R
+import br.edu.infnet.ucollect.dominio.modelos.Usuario
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_email_form.*
 
 class EmailFormActivity : AppCompatActivity() {
+
+    private lateinit var usuarioReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email_form)
 
+        usuarioReference = FirebaseDatabase.getInstance().reference
+            .child("usuarios").child("001")
         setListeners()
     }
 
@@ -21,22 +28,57 @@ class EmailFormActivity : AppCompatActivity() {
 
             var email = edtxt_email_form.text.toString()
             if (!email.isNullOrEmpty()){
-                var resultIntentRegistro = Intent(this@EmailFormActivity, RegistroActivity::class.java )
-                resultIntentRegistro.putExtra("email", email)
-                startActivity(resultIntentRegistro)
-                //finish()
-            } else{
+
+                val usuarioListener = object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val usuario = dataSnapshot.getValue(Usuario::class.java)
+                        // [START_EXCLUDE]
+                        usuario?.let {
+                            var nome = it.nome
+                            var apelido = it.apelido
+                            var email = it.email
+                        }
+                        // [END_EXCLUDE]
+                        if(usuario?.email == email){
+                            var resultIntentLogin = Intent(baseContext, LoginActivity::class.java )
+                            resultIntentLogin.putExtra(EXTRA_EMAIL_USUARIO, usuario.email)
+                            startActivity(resultIntentLogin)
+                            finish()
+
+                        }else{
+                            var resultIntentRegistro = Intent(baseContext, RegistroActivity::class.java )
+                            resultIntentRegistro.putExtra(EXTRA_CAMPO_EMAIL, email)
+                            startActivity(resultIntentRegistro)
+                            finish()
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+
+                        Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                        // [START_EXCLUDE]
+                        Toast.makeText(baseContext, "Failed to load post.",
+                            Toast.LENGTH_SHORT).show()
+                        // [END_EXCLUDE]
+                    }
+                }
+                usuarioReference.addValueEventListener(usuarioListener)
+
+            }else{
                 Toast.makeText(this, "Email é Obrigatório!", Toast.LENGTH_LONG).show()
             }
+
         }
 
-        btn_registrado_email_form.setOnClickListener {
-            var intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        btn_continuar_email_form.setOnClickListener {
+            finish()
         }
     }
 
-    override fun onResume() {
-        super.onResume()
+    companion object {
+
+        private const val TAG = "EmailFormActivity"
+        const val EXTRA_EMAIL_USUARIO = "emailUsuario"
+        const val EXTRA_CAMPO_EMAIL = "campoEmail"
     }
 }
