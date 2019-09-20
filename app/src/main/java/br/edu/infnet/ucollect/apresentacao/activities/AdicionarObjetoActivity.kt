a324a8b174af49f6a107ac9d5f4874e61e32dcd3
faspinheiro <faspinheiro@gmail.com>
2019-09-19 21:46:34 -0300

    Criar mensagem

--- app/src/main/java/br/edu/infnet/ucollect/apresentacao/activities/AdicionarObjetoActivity.kt
+++ app/src/main/java/br/edu/infnet/ucollect/apresentacao/activities/AdicionarObjetoActivity.kt
@@ -3,9 +3,11 @@ package br.edu.infnet.ucollect.apresentacao.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.Toast
 import br.edu.infnet.ucollect.R
 import br.edu.infnet.ucollect.dominio.modelos.Residuo
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthProvider
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_adicionar_objeto.*
@@ -53,14 +55,17 @@ class AdicionarObjetoActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            var residuo = Residuo(1,
-                add_residuo_textInput_titulo.editText?.text.toString(),
-                add_residuo_textInput_descricao.editText?.text.toString(),
-                currentUser.uid,
-                2)
+            bancoDados.getReference("residuos").push().key?.let {
 
-            bancoDados.getReference("Residuos").child(currentUser.uid).child(residuo.ResiduoId.toString()).setValue(residuo)
+                var residuo = Residuo(it,
+                    add_residuo_textInput_titulo.editText?.text.toString(),
+                    add_residuo_textInput_descricao.editText?.text.toString(),
+                    currentUser.uid,
+                    2)
 
+                bancoDados.getReference("Residuos").child(currentUser.uid).child(residuo.ResiduoId).setValue(residuo)
+
+            }
         }
 
         add_residuo_btn_cancelar.setOnClickListener {
--- app/src/main/java/br/edu/infnet/ucollect/infraestrutura/repositorios/ResiduoRepository.kt
+++ app/src/main/java/br/edu/infnet/ucollect/infraestrutura/repositorios/ResiduoRepository.kt
@@ -1,17 +1,55 @@
 package br.edu.infnet.ucollect.infraestrutura.repositorios
 
+import android.util.Log
 import br.edu.infnet.ucollect.R
 import br.edu.infnet.ucollect.dominio.modelos.Residuo
+import com.google.firebase.database.*
 
 class ResiduoRepository {
 
-    private val residuos = listOf<Residuo>(
-        Residuo(1, "Bateria", "XPTO1", "1", R.drawable.ic_battery_20_black_24dp),
-        Residuo(2, "Computador","XPTO2", "1", R.drawable.ic_computer_black_24dp),
-        Residuo(3, "Teclado","XPTO3", "2", R.drawable.ic_keyboard_black_24dp),
-        Residuo(4, "Mémoria","XPTO4", "5", R.drawable.ic_memory_black_24dp),
-        Residuo(5, "Smartphone","XPTO5", "8", R.drawable.ic_smartphone_black_24dp)
-    )
 
-    fun getResiduos() = residuos
+    private lateinit var residuos: List<Residuo>
+
+
+    /*  listOf<Residuo>(
+        Residuo("1", "Bateria", "XPTO1", "1", R.drawable.ic_battery_20_black_24dp),
+        Residuo("2", "Computador","XPTO2", "1", R.drawable.ic_computer_black_24dp),
+        Residuo("3", "Teclado","XPTO3", "2", R.drawable.ic_keyboard_black_24dp),
+        Residuo("4", "Mémoria","XPTO4", "5", R.drawable.ic_memory_black_24dp),
+        Residuo("5", "Smartphone","XPTO5", "8", R.drawable.ic_smartphone_black_24dp)
+    )*/
+
+    fun getResiduos() : List<Residuo>{
+
+        var bancoDados = FirebaseDatabase.getInstance().getReference()
+
+        var bancoDadosRef = bancoDados.child("Residuos")
+
+        residuos = mutableListOf()
+
+        bancoDadosRef.addChildEventListener(object: ChildEventListener {
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+                residuos.plus(p0.getValue(Residuo::class.java))
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+
+        return residuos
+    }
+
 }
